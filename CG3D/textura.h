3d73dcc5737510339f18f9981481e90750b607d5
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-12-07 00:15:44 -0300

    Update textura.h

--- CG3D/textura.h
+++ CG3D/textura.h
@@ -1,18 +1,17 @@
-#ifndef TEXTURA_H
-#define TEXTURA_H
+#ifndef QUADRADO_H
+#define QUADRADO_H
 
-Class Textura{
-    private:
-        int largura, altura, nCanais;
-        unsigned char *dados;
-        unsigned int id;
-    public:
-        Textura();
-        void     carregar(char *arquivo);
-        unsigned int get_id();
+class Quadrado {
+public:
+    Quadrado(float x, float y);
+    void desenha() const;
+    void mover(float novoX, float novoY);
+    void girar(float angulo);
 
+private:
+    float Xv1;
+    float Yv1;
+    float angle; // Novo membro para armazenar o ângulo de rotação
 };
 
 #endif
-
-
